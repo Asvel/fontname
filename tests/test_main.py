1bfd07b651296747577e2733d9458feff1fe8326
Asvel <fe.asvel@gmail.com>
2014-09-03 14:14:17 +0800

    Add test case

---
+++ tests/test_main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+from unittest import TestCase, main
+
+from fontname import guess_names
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestMain(TestCase):
+
+    def test_font_name(self):
+        count = 0
+        for root, dirs, files in os.walk(os.path.join(here, "fonts")):
+            for file in files:
+                count += 1
+                file_path = os.path.join(root, file)
+                font_name = " & ".join(guess_names(file_path))
+                file_name = os.path.splitext(file)[0]
+                self.assertEqual(font_name, file_name)
+        self.assertTrue(count > 0, "No test data, please put some font files in tests/fonts/")
+
+
+if __name__ == '__main__':
+    main()
